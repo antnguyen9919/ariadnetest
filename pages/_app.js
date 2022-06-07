1d477be4a485b73913f8b79e43febfa0f6c2260d
hasim7025 <kchsm34@gmail.com>
2022-06-07 15:16:42 +0200

    google tag manager v6

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,18 @@ import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 import Layout from '../components/Layout'
 import {AnimatePresence} from "framer-motion"
-
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
 
 
 
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-TLNNDCF' });
+  }, []);
+
   return(
    
       <Layout>
